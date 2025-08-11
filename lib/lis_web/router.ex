0ccba2d23bb019132e1bbf87802501e3a5d71b23
Max Wilson <io@maxwilson.dev>
2025-08-11 23:43:07 +0000

    feat: router

--- lib/lis_web/router.ex
+++ lib/lis_web/router.ex
@@ -18,6 +18,8 @@ defmodule LISWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+
+    resources "/persons", PersonController
   end
 
   # Other scopes may use custom stacks.
