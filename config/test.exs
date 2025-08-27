5b234aaf90f1b540c51202b5ff2d5720f6669b20
Max Wilson <io@maxwilson.dev>
2025-08-27 15:03:50 +0000

    Revert "feat: move dbs to release directory for mounting extant data to a release"
    This reverts commit 5a4423dd8a2b059fd4cbfdfec4c66ce4fa266207. Not sure this is useful.

--- config/test.exs
+++ config/test.exs
@@ -6,7 +6,7 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :lis, LIS.Repo,
-  database: Path.expand("../rel/overlays/db/test/lis.db", __DIR__),
+  database: Path.expand("../db/test/lis.db", __DIR__),
   pool_size: 5,
   pool: Ecto.Adapters.SQL.Sandbox
 
